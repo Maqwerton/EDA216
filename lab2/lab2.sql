31bef0496d5223d849748513615f471eb0d38dbf
martinjohansson93 <dat13mjo@student.lu.se>
2018-02-07 11:46:45 +0100

    Now I think it actually works to run the script

--- lab2/lab2.sql
+++ lab2/lab2.sql
@@ -3,34 +3,14 @@
 -- be dropped in arbitrary order.
 PRAGMA foreign_keys=OFF;
 DROP TABLE IF EXISTS users;
-...
 PRAGMA foreign_keys=ON;
 
 -- Create the tables.
 
 CREATE TABLE users (
-    username      varchar(20) not null,
+    username      varchar(20) not null PRIMARY KEY,
     adress        varchar(20) not null,
-    phone         varchar(20) not null,
-    primary key   (username)
-);
-
-CREATE TABLE reservation (
-  resNbr          int not null AUTO_INCREMENT,
-  ToSee           varchar(255) not null,
-  booker          varchar(20) not null
-  primary key     resNbr
-);
-
-CREATE TABLE performance (
-  id              int not null AUTO_INCREMENT,
-  location        varchar(255) not null,
-  movie           varchar(255) not null,
-  time            varchar(20) not null,
-  date            varchar(20) not null,
-  foreign key     (movie) REFERENCES movies(movie_name),
-  foreign key     (theater) REFERENCES theater(theater_name),
-  primary key     (location, date, movie)
+    phone         varchar(20) not null
 );
 
 CREATE TABLE movies (
@@ -38,26 +18,56 @@ CREATE TABLE movies (
   primary key     (movie_name)
 );
 
-CREATE TABLE theater (
-  theater_name    varchar(20) not null,
+CREATE TABLE theaters (
+  theater_name    varchar(20) not null PRIMARY KEY,
   adress          varchar(255) not null,
-  seats           integer(20) not null,
-  primary key     (theater_name)
+  seats           integer(20) not null
 );
 
+CREATE TABLE performance (
+  id              INTEGER PRIMARY KEY,
+  location        varchar(255) not null,
+  movie           varchar(255) not null,
+  theater         varchar(20) not null,
+  time            varchar(20) not null,
+  date            varchar(20) not null,
+  foreign key     (movie) REFERENCES movies(movie_name),
+  foreign key     (theater) REFERENCES theaters(theater_name)
+);
+
+
+CREATE TABLE reservation (
+  resNbr          INTEGER PRIMARY KEY,
+  to_see          varchar(255) not null,
+  booker          varchar(20) not null,
+  foreign key     (to_see) REFERENCES performance(id),
+  foreign key     (booker) REFERENCES users(username)
+);
 -- Insert data into the tables.
 INSERT
-INTO    users
-VALUES (Martin, hejvägen 51, 073057535)
-INöTO    users
-VALUES (Daniel, vägensväg 35, 53525325)
-INTO    reservation
-VALUES ( hejvägen 51, 073057535)
-INTO    users
-VALUES (Martin, hejvägen 51, 073057535)
-INTO    users
-VALUES (Martin, hejvägen 51, 073057535)
-INTO    users
-VALUES (Martin, hejvägen 51, 073057535)
+INTO users
+VALUES ("Martin", "hejvägen 51", "073057535");
 
-...
+INSERT
+INTO movies
+VALUES ("hejsan");
+
+INSERT
+INTO theaters
+VALUES ("Robins rum", "Robbans vag", 200);
+
+INSERT 
+INTO performance(location, movie, theater, time, date)
+VALUES ("Robins rum", "hejsan", "Robins rum", "20:20", "180102");
+
+INSERT
+INTO reservation(to_see, booker)
+VALUES (1, "Martin");
+
+INSERT
+INTO    users
+VALUES ("Martin43", "hejvägen 53", "073057535");
+
+INSERT
+INTO    users
+VALUES ("Martin65"," hejvägen 52", "073057535");
